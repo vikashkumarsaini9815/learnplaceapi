634593389a08dd8522a1635c80b506b4288d5fce
vikash kumar <vickykartoos1598@gmail.com>
2021-06-18 23:06:55 +0530

    2 file change the module

--- create_table_contact_us.py
+++ create_table_contact_us.py
@@ -5,3 +5,4 @@ mycursor = mydb.cursor()
 def create_table ():
        result =  mycursor.execute("CREATE TABLE contact_us(id INT (5) AUTO_INCREMENT PRIMARY KEY,username VARCHAR (200) ,contact INT (12),message VARCHAR (255))")
        return result
+create_table()
