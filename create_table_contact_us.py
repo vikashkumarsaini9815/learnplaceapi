45777888a9984ada5dfb2ba5c7da74b3112e5bdc
vikash kumar <vickykartoos1598@gmail.com>
2021-06-19 23:09:47 +0530

    database change remove uniqe from contact

--- create_table_contact_us.py
+++ create_table_contact_us.py
@@ -3,6 +3,6 @@ mydb= mysqldb.connector_mysql()
 
 mycursor = mydb.cursor()
 def create_table ():
-       result =  mycursor.execute("CREATE TABLE contact_us(id INT (5) AUTO_INCREMENT PRIMARY KEY,username VARCHAR (200) ,contact INT (12),message VARCHAR (255))")
+       result =  mycursor.execute("CREATE TABLE contact_us(id INT (5) AUTO_INCREMENT PRIMARY KEY,username VARCHAR (200) ,contact INT (255),message VARCHAR (255))")
        return result
 create_table()
--- product_module.py
+++ product_module.py
@@ -6,7 +6,7 @@ def create():
     c = "create table product" \
         "(product_id int(10) primary key auto_increment," \
         "product_name varchar(50)," \
-        "price int(10), " \
+        "price DECIMAL(10.2), " \
         "title varchar(50)," \
         "product_type varchar(50), " \
         "description varchar(255), " \
